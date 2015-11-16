276265272b4bd806cd1806895102b2de531027fc
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-11-16 14:44:38 -0500

    add tests for /login and /add-grocery

--- src/test/java/com/theironyard/GroceryTrackerApplicationTests.java
+++ src/test/java/com/theironyard/GroceryTrackerApplicationTests.java
@@ -1,18 +1,68 @@
 package com.theironyard;
 
+import com.theironyard.services.GroceryRepository;
+import com.theironyard.services.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = GroceryTrackerApplication.class)
 @WebAppConfiguration
 public class GroceryTrackerApplicationTests {
 
+	@Autowired
+	GroceryRepository groceries;
+
+	@Autowired
+	UserRepository users;
+
+	@Autowired
+	WebApplicationContext wap;
+
+	MockMvc mockMvc;
+
+	@Before
+	public void before() {
+		groceries.deleteAll();
+		users.deleteAll();
+		mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void testLogin() throws Exception {
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/login")
+					.param("username", "testu")
+					.param("password", "testp")
+		);
+		assertTrue(users.count() == 1);
+	}
+
+
+	@Test
+	public void testAddGrocery() throws Exception {
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/add-grocery")
+					.param("name", "TestBeer")
+					.param("brand", "TestBrand")
+					.param("quantity", "10")
+					.param("quantityType", "Pound(s)")
+					.param("category", "Red Meat")
+					.sessionAttr("username", "testu")
+		);
+
+		assertTrue(groceries.count() == 1);
 	}
 
 }
